05595ffa9365dba0e53510191ed9f124ce2293a1
joamag <joamag@hive.pt>
2020-03-21 12:45:03 +0000

    New MVC selection

--- src/hive_blog/system.py
+++ src/hive_blog/system.py
@@ -63,7 +63,7 @@ class HiveBlog(colony.System):
         This load should occur only after the dependencies are loaded.
         """
 
-        # retrieves the mvc utils plugin
+        # retrieves the MVC utils plugin
         mvc_utils_plugin = self.plugin.mvc_utils_plugin
 
         # retrieves the entity manager arguments
@@ -80,7 +80,7 @@ class HiveBlog(colony.System):
         This load should occur the earliest possible in the unloading process.
         """
 
-        # retrieves the mvc utils plugin
+        # retrieves the MVC utils plugin
         mvc_utils_plugin = self.plugin.mvc_utils_plugin
 
         # retrieves the entity manager arguments
@@ -94,12 +94,12 @@ class HiveBlog(colony.System):
     def get_patterns(self):
         """
         Retrieves the tuple of regular expressions to be used as patterns,
-        to the mvc service. The tuple should relate the route with the handler
+        to the MVC service. The tuple should relate the route with the handler
         method/function.
 
         :rtype: Tuple
         :return: The tuple of regular expressions to be used as patterns,
-        to the mvc service.
+        to the MVC service.
         """
 
         return (
@@ -127,12 +127,12 @@ class HiveBlog(colony.System):
     def get_resource_patterns(self):
         """
         Retrieves the tuple of regular expressions to be used as resource patterns,
-        to the mvc service. The tuple should relate the route with the base
+        to the MVC service. The tuple should relate the route with the base
         file system path to be used.
 
         :rtype: Tuple
         :return: The tuple of regular expressions to be used as resource patterns,
-        to the mvc service.
+        to the MVC service.
         """
 
         # retrieves the plugin manager and uses it to retrieve
@@ -155,11 +155,11 @@ class HiveBlog(colony.System):
         process for the retrieval of the arguments.
         """
 
-        # retrieves the mvc utils plugin
+        # retrieves the MVC utils plugin
         mvc_utils_plugin = self.plugin.mvc_utils_plugin
 
         # generates the entity manager arguments, creating the proper structures
-        # using the standard mvc implementation and returns the values
+        # using the standard MVC implementation and returns the values
         entity_manager_arguments = mvc_utils_plugin.generate_entity_manager_arguments(
             self.plugin, ENTITY_MANAGER_ARGUMENTS, ENTITY_MANAGER_PARAMETERS
         )
